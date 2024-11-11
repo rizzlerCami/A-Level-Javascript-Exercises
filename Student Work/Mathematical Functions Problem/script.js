1d7ca8c47b2ac19d9065d66d9fddc41e77c15217
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2024-11-11 10:14:50 +0000

    jacob elordi

--- Student Work/Mathematical Functions Problem/script.js
+++ Student Work/Mathematical Functions Problem/script.js
@@ -33,15 +33,24 @@ function cylArea() {
 // Function to calculate the volume of a cone
 function cone() {
     // TODO: Write the formula for cone volume
-    area = 2**65
+    radius = document.getElementById("coneRadius").value
+    height = document.getElementById("coneHeight").value
+    output3.innerHTML = `Volume = ${Math.PI * radius**2 * height / 3}`
 }
 
 // Function to calculate the hypotenuse using Pythagoras' theorem
-function _________________________________() {
+function pythag() {
     // TODO: Write the formula for hypotenuse
+    base1 = document.getElementById("sideA").value
+    base2 = document.getElementById("sideB").value
+    output4.innerHTML = `Hypotenuse = ${(base1**2 + base2**2)**(1/2)}`
 }
 
 // Function to calculate the area of a triangle using Hero's formula
-function _________________________________() {
+function HERO() {
     // TODO: Write the formula for Hero's area
+    base1 = document.getElementById("side1").value
+    base2 = document.getElementById("side2").value
+    height = document.getElementById("side3").value
+    output5.innerHTML = `Area = ${(((Number(base1) + Number(base2) + Number(height)) / 2)*((Number(base1) + Number(base2) + Number(height)) / 2 - base1)*((Number(base1) + Number(base2) + Number(height)) / 2 - base2)*((Number(base1) + Number(base2) + Number(height)) / 2 - height))**(1/2)}`
 }
