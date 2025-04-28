414bf9793ac405942b7d10858bd05af7cb81d3a7
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2025-04-28 10:32:13 +0100

    h

--- Class Exercises/8 - Sorting Algorithms/Bubble Sort/Bubble Sort Assess/script.js
+++ Class Exercises/8 - Sorting Algorithms/Bubble Sort/Bubble Sort Assess/script.js
@@ -36,8 +36,8 @@ document.addEventListener("DOMContentLoaded", function() {
             for (let i = 0; i < n - 1; i++) {
                 if (array[i] > array[i + 1]) {
                     temp = array[i]
-                    array[i] = array[i+1]
-                    array[i+1] = temp
+                    array[i] = array[i + 1]
+                    array[i + 1] = temp
                     swap = true
                 }
             }
