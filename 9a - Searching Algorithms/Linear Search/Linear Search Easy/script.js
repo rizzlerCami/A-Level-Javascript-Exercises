dd7e12df2ec9a4f26b27692d8e2234519d25c4ba
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2025-05-12 14:54:35 +0100

    es

--- 9a - Searching Algorithms/Linear Search/Linear Search Easy/script.js
+++ 9a - Searching Algorithms/Linear Search/Linear Search Easy/script.js
@@ -51,7 +51,7 @@ function insertionSort(array) {
     for (let i = 1; i < array.length; i++) {
         let j = i - 1
         let current = array[i]
-        while (i >= 0 && array[j] > current) {
+        while (j >= 0 && array[j] > current) {
             array[j + 1] = array[j]
             j--
         }
